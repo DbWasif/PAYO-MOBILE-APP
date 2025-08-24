1239d102f6a3285a25133e9cb782e6a17e8b2aff
mywasif <mdwasif01300@gmail.com>
2025-08-24 13:38:09 +0600

    some updated home.js

--- home.js
+++ home.js
@@ -1,13 +1,21 @@
+const validPin = 1234;
 document.getElementById("add-money-btn").addEventListener("click", function(e) {
     e.preventDefault();
 
     const bank = document.getElementById("bank").value;
     const accountNumber = document.getElementById("account-number").value;
     const amount = parseInt(document.getElementById("add-amount").value);
-    const pin = document.getElementById("add-pin").value;
+    const pin = parseInt(document.getElementById("add-pin").value);
     const availableBalance = parseInt(document.getElementById("available-balance").innerText);
     
+    if(accountNumber.length < 11) {
+        alert("Please provide valid account number")
+        return;
+    }
+    if(pin !== validPin) {
+        alert("Plaese provide valid pin number")
+        return;
+    }
     const totalNewAvailabelBalance = amount + availableBalance;
-
     document.getElementById("available-balance").innerText = totalNewAvailabelBalance;
 })
